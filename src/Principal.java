7139858b6eee6a8cdfa580b5f3ab4de887a8b6de
Balam <balcaraza@gmail.com>
2024-09-21 22:43:14 -0600

    se añade interfaz y filtro de recomendacion

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.calculos.FiltroRecomendacion;
+import com.aluracursos.screenmatch.modelos.Episodio;
 import com.aluracursos.screenmatch.modelos.Pelicula;
 import com.aluracursos.screenmatch.modelos.Serie;
 
@@ -41,7 +43,16 @@ public class Principal {
        System.out.println("Tiempo estimado para ver los titulos elegidos: " + calculadora.getTiempoTotal() + " minutos");
 
 
+        FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
+        filtroRecomendacion.filtra(miPelicula);
 
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setNombre("La casa de los famosos");
+        episodio.setSerie(casaDragon);
+        episodio.setTotalVisualizaciones(50);
+
+        filtroRecomendacion.filtra(episodio);
 
 
     }
